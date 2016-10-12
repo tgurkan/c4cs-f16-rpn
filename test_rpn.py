d37335c63415a245f85d56c434d186b8524c6459
tgurkan <tgurkan@umich.edu>
2016-10-12 18:24:53 -0400

    Power function

--- test_rpn.py
+++ test_rpn.py
@@ -6,6 +6,9 @@ class TestBasics(unittest.TestCase):
 	def test_add(self):
 		result = rpn.calculate("1 1 +")
 		self.assertEqual(2, result)
+	def test_exp(self):
+		result = rpn.calculate("2 2 ^")
+		self.assertEqual(4, result)
 	def test_subtract(self):
 		result = rpn.calculate("5 3 -")
 		self.assertEqual(2, result)
